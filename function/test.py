09490c8d2175dd1393ef767669d413913edcab7d
hein <heinrichcapatina958@gmail.com>
2025-04-25 23:00:23 +0800

    other merge issue

--- function/test.py
+++ function/test.py
@@ -7,8 +7,5 @@ print(tan(pi / 4) * tan(pi / 2))
 """
 this is a new comment
 """
-<<<<<<< HEAD
 print("add a new line")
-=======
 print("this is a new line")
->>>>>>> 1de4080425628fe123bd13c3645f9e83a285b037
